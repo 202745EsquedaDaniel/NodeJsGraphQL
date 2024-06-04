e3672203e5d14c4b0442b26ae632617aa50e5c5e
00lar <eduardani84@gmail.com>
2024-06-04 14:14:55 -0600

    archives graphql and watch for them

--- src/graphQL/index.js
+++ src/graphQL/index.js
@@ -1,36 +1,8 @@
 const { ApolloServer} = require("apollo-server-express")
 const { ApolloServerPluginLandingPageGraphQLPlayground} = require("apollo-server-core")
+const { loadFiles} = require("@graphql-tools/load-files") //instalar graphql-tools con npm install @graphql-tools/load-files
 
-const typeDefs = `
-  type Query {
-    hello: String!
-    getPerson(name: String, age: Int): String
-    getInt(age: Int): Int
-    getFloat(price: Float): Float
-    getString: String
-    getBoolean: Boolean
-    getID: ID
-    getNumbers(numbers: [Int!]!): [Int]
-    getProduct: Product
-  }
 
-  type Product {
-    id: ID!,
-    name: String!,
-    price: Float!,
-    description: String!,
-    image: String!,
-    createdAt: String!
-  }
-`;
-
-// El agregar el signo de exclamación al final de cada tipo de dato significa que no pueede ser nulo
-//Get = Query
-//Post, Delete and Patch = Mutation
-
-//Lists
-// [String] = List of strings
-// [Int] = List of integers
 
 const resolvers = {
   Query: {
@@ -57,7 +29,7 @@ const resolvers = {
 
 const useGraphql = async (app) => {
   const server = new ApolloServer({
-    typeDefs,
+    typeDefs: await loadFiles("./src/**/*.graphql"),
     resolvers,
     playground: true,
     plugins : [
