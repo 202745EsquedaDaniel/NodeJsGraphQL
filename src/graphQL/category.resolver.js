2b7399dafdec45cf98d8c09dcda1a12ca842b0b0
00lar <eduardani84@gmail.com>
2024-06-10 12:41:12 -0600

    jwt

--- src/graphQL/category.resolver.js
+++ src/graphQL/category.resolver.js
@@ -1,9 +1,14 @@
 const categoryService = require("../services/category.service")
 const service = new categoryService();
+const boom = require("@hapi/boom")
 
-const addCategory = async(_, {dto}) => {
+const addCategory = async (_, {dto}, context) => {
+  const { user } = await context.authenticate("jwt", {session: false})
+  if (!user) {
+    throw boom.unauthorized("jwt not valid")
+  }
   const newCategory = await service.create(dto)
   return newCategory
 }
 
-module.exports = { addCategory}
+module.exports = { addCategory }
